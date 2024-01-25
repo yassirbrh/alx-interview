1be7b4283dcd3fbf4956cd3dec876cd353947397
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-25 22:33:01 +0000

    Script that reads stdin line by line and computes metrics.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -44,31 +44,30 @@ def show_status_code_states(status_codes, files_total_size):
             print("{:s}: {:d}".format(code, status_codes[code]))
 
 
-if __name__ == '__main__':
-    try:
-        files_total_size = 0
-        number_of_lines = 0
-        status_codes_states = {
-            '200': 0,
-            '301': 0,
-            '400': 0,
-            '401': 0,
-            '403': 0,
-            '404': 0,
-            '405': 0,
-            '500': 0,
-        }
-        for line in sys.stdin:
-            if check_line(line):
-                file_size = line.split(' ')[8]
-                if file_size == '\n':
-                    file_size = file_size[:-1]
-                status_code = line.split(' ')[7]
-                if status_code in sorted(status_codes_states.keys()):
-                    status_codes_states[status_code] += 1
-                files_total_size += int(file_size)
-            number_of_lines += 1
-            if number_of_lines % 10 == 0:
-                show_status_code_states(status_codes_states, files_total_size)
-    finally:
-        show_status_code_states(status_codes_states, files_total_size)
+try:
+    files_total_size = 0
+    number_of_lines = 0
+    status_codes_states = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    for line in sys.stdin:
+        if check_line(line):
+            file_size = line.split(' ')[8]
+            if file_size == '\n':
+                file_size = file_size[:-1]
+            status_code = line.split(' ')[7]
+            if status_code in sorted(status_codes_states.keys()):
+                status_codes_states[status_code] += 1
+            files_total_size += int(file_size)
+        number_of_lines += 1
+        if number_of_lines % 10 == 0:
+            show_status_code_states(status_codes_states, files_total_size)
+finally:
+    show_status_code_states(status_codes_states, files_total_size)
