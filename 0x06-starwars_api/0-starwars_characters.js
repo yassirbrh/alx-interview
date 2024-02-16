6edca1a679798d1d160251a508016cd119fe3dd0
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-16 22:49:46 +0000

    Script that prints all characters of a Star Wars movie using request module.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,20 +5,28 @@ const id = process.argv[2];
 if (process.argv.length > 2) {
   request(url + id, (err, resp, body) => {
     if (err) {
-      console.log(err);
+      console.error(err);
+      return;
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, promiseResp, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+
+    try {
+      const charactersURL = JSON.parse(body).characters;
+      const charactersName = charactersURL.map(
+        url => new Promise((resolve, reject) => {
+          request(url, (promiseErr, promiseResp, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+              return;
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        }));
+
+      Promise.all(charactersName)
+        .then(names => console.log(names.join('\n')))
+        .catch(allErr => console.error(allErr));
+    } catch (parseErr) {
+      console.error('Error parsing response:', parseErr);
+    }
   });
 }
